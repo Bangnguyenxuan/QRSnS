ea38031a1364f6aa6f4ee0baf2a0f7f759f2df78
XuanBang <bang.nguyenxuan0101@gmail.com>
2015-08-16 10:30:16 +0700

    Initial Project

---
+++ pc-index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UNited We Hack</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <img class="bg" src="img/bg.jpg" alt="" />
+        <div id="wrapper" class="container-fluid" >
+            <div id="wellcome" class="text-center">
+                <img src="img/logo.png" alt="" />
+                <h3>UNited We Hack, August 2015</h3>
+                <h4>Save and Safe</h4>
+                <div id="login-form">
+                    <form class="form-signin">
+                            <input type="text" class="form-control" placeholder="Email address" autofocus>
+                            <input type="password" class="form-control" placeholder="Password">
+                    </form>
+                    <button class="btn btn-lg btn-primary btn-block btnSignin">Sign in</button>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript">
+            $(document).ready(function(){
+            //$('.item').each(function(){
+                $('.btnSignin').click(function(){
+                    $('#wellcome').fadeOut('slow',function(){
+                        location.href = 'tab.php';
+                    });
+                });
+            //});
+            });
+        </script>
+    </body>
+</html>
